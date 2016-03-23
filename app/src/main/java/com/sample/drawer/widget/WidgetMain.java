2b4f1dd6556a5b5886e1b1ad77da8e97a14b3e3f
Sergey.Atroschenko <Sergey.Atroschenko@r.noveogroup.com>
2016-03-23 19:04:57 +0600

    started doing widget

---
+++ app/src/main/java/com/sample/drawer/widget/WidgetMain.java
@@ -0,0 +1,12 @@
+package com.sample.drawer.widget;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by admin on 3/23/2016.
+ */
+public class WidgetMain extends AppWidgetProvider {
+
+
+
+}
