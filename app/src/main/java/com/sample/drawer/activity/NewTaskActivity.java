5751f85e67221daa115dc3c9693b72a42bdaad2d
Sergey.Atroschenko <Sergey.Atroschenko@r.noveogroup.com>
2016-03-29 14:37:40 +0600

    added select date create new task

--- app/src/main/java/com/sample/drawer/activity/NewTaskActivity.java
+++ app/src/main/java/com/sample/drawer/activity/NewTaskActivity.java
@@ -1,16 +1,19 @@
 package com.sample.drawer.activity;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.sample.drawer.R;
+import com.sample.drawer.fragments.DateDialog;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -25,6 +28,8 @@ public class NewTaskActivity extends AppCompatActivity {
     private static final String DEADLINE_INTENT_KEY = "deadline_intent_key";
     private static final String INFO_INTENT_KEY = "info_intent_key";
 
+    private final static String DATAPICKER_KEY = "datePicker";
+
     @Bind(R.id.add_task_lesson)
     EditText addTaskLesson;
     @Bind(R.id.add_task_info)
@@ -33,6 +38,8 @@ public class NewTaskActivity extends AppCompatActivity {
     TextView addTaskDeadline;
     @Bind(R.id.toolbar_add_new_task)
     Toolbar toolbar;
+
+    DateDialog newFragment;
 
 
     @Override
@@ -65,6 +72,18 @@ public class NewTaskActivity extends AppCompatActivity {
         }
     }
 
+    @OnClick(R.id.add_task_deadline)
+    public void clickTextViewDeadline() {
+        newFragment = new DateDialog();
+        newFragment.setOnDateSetListener(new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(final DatePicker datePicker, final int year, final int monthOfYear, final int dayOfMonth) {
+                String[] mounth = getResources().getStringArray(R.array.mount_list);
+                addTaskDeadline.setText(getString(R.string.format_date, dayOfMonth, mounth[monthOfYear], year));
+            }
+        });
+        newFragment.show(getFragmentManager(), DATAPICKER_KEY);
+    }
 
     @OnClick(R.id.add_task_button_save)
     public void clickToButtonSave() {
