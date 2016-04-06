3b6c36dc6b5e552ad8028aba8f0f7937e0080bcd
serg <serg3z.hard@gmail.com>
2016-04-06 22:39:18 +0600

    added floating action menu

--- app/src/main/java/com/sample/drawer/activity/NewLessonActivity.java
+++ app/src/main/java/com/sample/drawer/activity/NewLessonActivity.java
@@ -1,28 +1,20 @@
 package com.sample.drawer.activity;
 
-import android.app.FragmentManager;
 import android.app.DialogFragment;
+import android.app.FragmentManager;
 import android.app.LoaderManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
 import android.os.Bundle;
-import android.support.design.widget.CoordinatorLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v4.view.ViewCompat;
+import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.widget.ThemedSpinnerAdapter;
-import android.util.AttributeSet;
-import android.util.Log;
-import android.view.View;
-import android.view.animation.LinearInterpolator;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.github.clans.fab.FloatingActionButton;
 import com.j256.ormlite.android.apptools.OrmLitePreparedQueryLoader;
-import com.j256.ormlite.android.apptools.OrmLiteQueryForAllLoader;
 import com.sample.drawer.R;
 import com.sample.drawer.database.Classroom;
 import com.sample.drawer.database.Day;
@@ -36,9 +28,8 @@ import com.sample.drawer.database.Subject;
 import com.sample.drawer.database.Teacher;
 import com.sample.drawer.database.dao.DayDAO;
 import com.sample.drawer.database.dao.DayPeriodDAO;
-import com.sample.drawer.database.dao.PeriodDAO;
-import com.sample.drawer.database.dao.TeacherDAO;
 import com.sample.drawer.database.loader.OrmLiteQueryForAllOrderByLoader;
+
 import com.sample.drawer.fragments.schedule.AddTimeDialogFragment;
 import com.sample.drawer.fragments.schedule.AddValueDialogFragment;
 
@@ -54,21 +45,28 @@ import butterknife.OnClick;
  */
 public class NewLessonActivity extends ActionBarActivity {
 
-    @Bind(R.id.time_lesson) Spinner time;
-    @Bind(R.id.fio_teacher) Spinner fioTeacher;
-    @Bind(R.id.name_lesson) Spinner nameLesson;
-    @Bind(R.id.number_auditory) Spinner numberAuditory;
-    @Bind(R.id.type_lesson) Spinner typeLesson;
-    @Bind(R.id.type_week) Spinner typeWeek;
-    @Bind(R.id.button_delete) Button deleteBtn;
-    FragmentManager fragmentManager;
     public static final String ARG_DAY_OF_WEEK = "day_of_week";
     public static final String ARG_LESSON_ID = "lesson_id";
     public static final String TAG_DIALOG = "dialog";
     public static final int LOADER_ADD_LESSON = 10;
     public static final int LOADER_EDIT_LESSON = 11;
     public static final int LOADER_DELETE_LESSON = 11;
+    @Bind(R.id.time_lesson)
+    Spinner time;
+    @Bind(R.id.fio_teacher)
+    Spinner fioTeacher;
+    @Bind(R.id.name_lesson)
+    Spinner nameLesson;
+    @Bind(R.id.number_auditory)
+    Spinner numberAuditory;
+    @Bind(R.id.type_lesson)
+    Spinner typeLesson;
+    @Bind(R.id.type_week)
+    Spinner typeWeek;
+    @Bind(R.id.button_delete)
+    FloatingActionButton buttonDelete;
 
+    FragmentManager fragmentManager;
     private List<Subject> lessonList;
     private List<Teacher> teacherList;
     private List<PeriodTime> timeList;
@@ -80,14 +78,14 @@ public class NewLessonActivity extends ActionBarActivity {
 
 
     //добавление пары для дня недели
-    public static Intent newAddIntent(Context context, int dayOfWeek){
+    public static Intent newAddIntent(Context context, int dayOfWeek) {
         Intent intent = new Intent(context, NewLessonActivity.class);
         intent.putExtra(ARG_DAY_OF_WEEK, dayOfWeek);
         return intent;
     }
 
     //редактирование существующей пары
-    public static Intent newEditIntent(Context context, int dayOfWeek, int lessonID){
+    public static Intent newEditIntent(Context context, int dayOfWeek, int lessonID) {
         Intent intent = new Intent(context, NewLessonActivity.class);
         intent.putExtra(ARG_LESSON_ID, lessonID);
         intent.putExtra(ARG_DAY_OF_WEEK, dayOfWeek);
@@ -101,10 +99,11 @@ public class NewLessonActivity extends ActionBarActivity {
         ButterKnife.bind(this);
         loadLists();
 
-        lessonID = getIntent().getIntExtra(ARG_LESSON_ID,0);
-        dayOfWeek = getIntent().getIntExtra(ARG_DAY_OF_WEEK,0);
+
+        lessonID = getIntent().getIntExtra(ARG_LESSON_ID, 0);
+        dayOfWeek = getIntent().getIntExtra(ARG_DAY_OF_WEEK, 0);
         if (lessonID == 0){
-            deleteBtn.setEnabled(false);
+            buttonDelete.setEnabled(false);
         }
     }
 
@@ -113,12 +112,6 @@ public class NewLessonActivity extends ActionBarActivity {
         super.onDestroy();
         ButterKnife.unbind(this);
     }
-
-    @OnClick(R.id.fab)
-    public void clickFab(){
-        
-    }
-
 
     @OnClick(R.id.button_cancel)
     public void onClickButtonCancel(){
@@ -156,8 +149,8 @@ public class NewLessonActivity extends ActionBarActivity {
         });
     }
 
-    public void removeLesson(List<DayPeriod> dayPeriods){
-        for (DayPeriod dp : dayPeriods){
+    public void removeLesson(List<DayPeriod> dayPeriods) {
+        for (DayPeriod dp : dayPeriods) {
             try {
                 HelperFactory.getHelper().getDayPeriodDAO().delete(dp);
             } catch (SQLException e) {
@@ -222,9 +215,9 @@ public class NewLessonActivity extends ActionBarActivity {
 
     }
 
-    private void addLessonToSchedule(final Period period){
+    private void addLessonToSchedule(final Period period) {
         final Bundle bundle = new Bundle();
-        bundle.putInt(ARG_DAY_OF_WEEK,dayOfWeek);
+        bundle.putInt(ARG_DAY_OF_WEEK, dayOfWeek);
         getLoaderManager().initLoader(LOADER_ADD_LESSON, bundle, new LoaderManager.LoaderCallbacks<List<Day>>() {
             @Override
             public Loader<List<Day>> onCreateLoader(int id, Bundle args) {
@@ -241,7 +234,7 @@ public class NewLessonActivity extends ActionBarActivity {
 
             @Override
             public void onLoadFinished(Loader<List<Day>> loader, List<Day> data) {
-                addLessonToDays(period,data);
+                addLessonToDays(period, data);
                 finish();
             }
 
@@ -253,10 +246,10 @@ public class NewLessonActivity extends ActionBarActivity {
     }
 
 
-    private void updateLesson(final Period period){
+    private void updateLesson(final Period period) {
         final Bundle bundle = new Bundle();
-        bundle.putInt(ARG_DAY_OF_WEEK,dayOfWeek);
-        bundle.putInt(ARG_LESSON_ID,lessonID);
+        bundle.putInt(ARG_DAY_OF_WEEK, dayOfWeek);
+        bundle.putInt(ARG_LESSON_ID, lessonID);
         getLoaderManager().initLoader(LOADER_EDIT_LESSON, bundle, new LoaderManager.LoaderCallbacks<List<Day>>() {
             int lessonID;
 
@@ -293,7 +286,7 @@ public class NewLessonActivity extends ActionBarActivity {
         });
     }
 
-    private void addLessonToDays(Period period, List<Day> days){
+    private void addLessonToDays(Period period, List<Day> days) {
         if (days != null) {
             for (Day d : days) {
                 boolean add = true;
@@ -313,12 +306,43 @@ public class NewLessonActivity extends ActionBarActivity {
     }
 
 
-    private void loadLists(){
+    private void loadLists() {
         getLoaderManager().initLoader(ListLoader.LOADER_LESSONS, null, new ListLoader());
         getLoaderManager().initLoader(ListLoader.LOADER_AUDITORIES, null, new ListLoader());
         getLoaderManager().initLoader(ListLoader.LOADER_TIMES, null, new ListLoader());
         getLoaderManager().initLoader(ListLoader.LOADER_TEACHERS, null, new ListLoader());
         getLoaderManager().initLoader(ListLoader.LOADER_LESSON_TYPES, null, new ListLoader());
+    }
+
+    @OnClick(R.id.btn_add_fio_teacher)
+    void showAddTeacherDialog() {
+        showAddDialog(getString(R.string.fio_teacher));
+    }
+
+    @OnClick(R.id.btn_add_name_lesson)
+    void showAddLessonDialog() {
+        showAddDialog(getString(R.string.name_lesson));
+    }
+
+    @OnClick(R.id.btn_add_number_auditory)
+    void showAddAuditoryDialog() {
+        showAddDialog(getString(R.string.number_auditory));
+    }
+
+    @OnClick(R.id.btn_add_type_lesson)
+    void showAddLessonTypeDialog() {
+        showAddDialog(getString(R.string.type_lesson));
+    }
+
+    @OnClick(R.id.btn_add_time)
+    void showAddTimeDialog() {
+        DialogFragment newFragment = AddTimeDialogFragment.newInstance();
+        newFragment.show(getFragmentManager(), TAG_DIALOG);
+    }
+
+    private void showAddDialog(String arg) {
+        DialogFragment newFragment = AddValueDialogFragment.newInstance(arg);
+        newFragment.show(getFragmentManager(), TAG_DIALOG);
     }
 
     enum WeekType {BOTH, EVEN, ODD}
@@ -387,34 +411,6 @@ public class NewLessonActivity extends ActionBarActivity {
         @Override
         public void onLoaderReset(Loader loader) {
         }
-    }
-
-
-    @OnClick(R.id.btn_add_fio_teacher)
-    void showAddTeacherDialog(){
-        showAddDialog(getString(R.string.fio_teacher));
-    }
-    @OnClick(R.id.btn_add_name_lesson)
-    void showAddLessonDialog(){
-        showAddDialog(getString(R.string.name_lesson));
-    }
-    @OnClick(R.id.btn_add_number_auditory)
-    void showAddAuditoryDialog(){
-        showAddDialog(getString(R.string.number_auditory));
-    }
-    @OnClick(R.id.btn_add_type_lesson)
-    void showAddLessonTypeDialog(){
-        showAddDialog(getString(R.string.type_lesson));
-    }
-    @OnClick(R.id.btn_add_time)
-    void showAddTimeDialog(){
-        DialogFragment newFragment = AddTimeDialogFragment.newInstance();
-        newFragment.show(getFragmentManager(), TAG_DIALOG);
-    }
-
-    private void showAddDialog(String arg){
-        DialogFragment newFragment = AddValueDialogFragment.newInstance(arg);
-        newFragment.show(getFragmentManager(), TAG_DIALOG);
     }
 
 }
